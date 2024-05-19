007761ea38b2eac8cad89196ff1eb0ecb411c137
Alena Birulia <birulia.ip@gmail.com>
2024-05-19 20:20:55 +0300

    slider

--- index.js
+++ index.js
@@ -2,101 +2,102 @@
 //https://swiperjs.com
 
 let swiper = new Swiper('.swiper_about', {
-    // Optional parameters
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
+  // Optional parameters
+  loop: true,
 
-  let swiper2 = new Swiper('.swiper_teachers', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 3,
-    // effect: coverflow,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-  let swiper3 = new Swiper('.swiper_reviews', {
-    // Optional parameters
-    loop: true,
+});
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
+let swiper2 = new Swiper('.swiper_teachers', {
+  // Optional parameters
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+  // effect: coverflow,
 
-  //details
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-  function onToggle(event) {
-    if (event.target.open) {
-      document.querySelectorAll(".questions__table_item > details[open]").forEach((el) => {
-        // Исключаем из перебора елемент который мы только что открыли
-        if (el === event.target) {
-          return;
-        }
-  
-        // Закрываем все остальные елементы <details>
-        el.open = false;
-      });
-    }
+});
+
+let swiper3 = new Swiper('.swiper_reviews', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+//details
+
+function onToggle(event) {
+  if (event.target.open) {
+    document.querySelectorAll(".questions__table_item > details[open]").forEach((el) => {
+      // Исключаем из перебора елемент который мы только что открыли
+      if (el === event.target) {
+        return;
+      }
+
+      // Закрываем все остальные елементы <details>
+      el.open = false;
+    });
   }
-  
-  // Вешаем наблюдатель на все елементы <details> внутри акордиона
-  document
-    .querySelectorAll(".questions__table_item > details")
-    .forEach((el) => el.addEventListener("toggle", onToggle));
+}
+
+// Вешаем наблюдатель на все елементы <details> внутри акордиона
+document
+  .querySelectorAll(".questions__table_item > details")
+  .forEach((el) => el.addEventListener("toggle", onToggle));
 
 //ленивая загрузка 
 
 const images = document.querySelectorAll('img[data-src]');
 const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.2
+root: null,
+rootMargin: '0px',
+threshold: 0.2
 };
 
 const imageObserver = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const lazyImage = entry.target;
-      lazyImage.src = lazyImage.dataset.src;
-      lazyImage.classList.remove('lazy');
-      observer.unobserve(lazyImage);
-    }
-  });
+entries.forEach(entry => {
+  if (entry.isIntersecting) {
+    const lazyImage = entry.target;
+    lazyImage.src = lazyImage.dataset.src;
+    lazyImage.classList.remove('lazy');
+    observer.unobserve(lazyImage);
+  }
+});
 }, options);
 
 images.forEach(image => {
-  imageObserver.observe(image);
+imageObserver.observe(image);
 });
 
 //ленивая загрузка видео 
 function init() {
-  var vidDefer = document.getElementsByTagName('iframe');
-  for (var i=0; i<vidDefer.length; i++) {
-      if(vidDefer[i].getAttribute('data-src')) {
-          vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
-          }
-      } 
-  }
-  window.onload = init;
+var vidDefer = document.getElementsByTagName('iframe');
+for (var i=0; i<vidDefer.length; i++) {
+    if(vidDefer[i].getAttribute('data-src')) {
+        vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+        }
+    } 
+}
+window.onload = init;
